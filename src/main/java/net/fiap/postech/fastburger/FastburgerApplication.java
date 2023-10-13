f096fdc21ec7cd21fe6d254b26aa1713e30ee352
Felipe Santos <fsantosmg@gmail.com>
2023-10-13 16:22:39 -0300

    projeto versão inicial

---
+++ src/main/java/net/fiap/postech/fastburger/FastburgerApplication.java
@@ -0,0 +1,13 @@
+package net.fiap.postech.fastburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastburgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastburgerApplication.class, args);
+    }
+
+}

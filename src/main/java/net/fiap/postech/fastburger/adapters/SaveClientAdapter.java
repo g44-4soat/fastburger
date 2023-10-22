3909278abce3469cbd3054314833c2b8ea0dd47f
anisberto reis <anisbertoos@gmail.com>
2023-10-22 18:44:19 -0300

    configuração do adapter - ajuste do controller

--- src/main/java/net/fiap/postech/fastburger/adapters/SaveClientAdapter.java
+++ src/main/java/net/fiap/postech/fastburger/adapters/SaveClientAdapter.java
@@ -3,12 +3,13 @@ package net.fiap.postech.fastburger.adapters;
 import net.fiap.postech.fastburger.adapters.persistence.mapper.ClientMapper;
 import net.fiap.postech.fastburger.adapters.persistence.repositories.ClientRepository;
 import net.fiap.postech.fastburger.application.domain.Client;
+import net.fiap.postech.fastburger.application.ports.inputports.client.SaveClientGateway;
 import net.fiap.postech.fastburger.application.ports.outputports.client.SaveClientOutPutPort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SaveClientAdapter implements SaveClientOutPutPort {
+public class SaveClientAdapter implements SaveClientGateway {
     private final ClientRepository clientRepository;
     private final ClientMapper clientMapper;
 
--- src/main/java/net/fiap/postech/fastburger/application/usecases/ClientUseCase.java
+++ src/main/java/net/fiap/postech/fastburger/application/usecases/ClientUseCase.java
@@ -1,10 +1,12 @@
 package net.fiap.postech.fastburger.application.usecases;
 
 import net.fiap.postech.fastburger.application.domain.Client;
+import net.fiap.postech.fastburger.application.ports.inputports.client.FindClientByCpfGateway;
+import net.fiap.postech.fastburger.application.ports.inputports.client.SaveClientGateway;
 import net.fiap.postech.fastburger.application.ports.outputports.client.FindClientByCpfOutPutPort;
 import net.fiap.postech.fastburger.application.ports.outputports.client.SaveClientOutPutPort;
 
-public class ClientUseCase {
+public class ClientUseCase implements SaveClientGateway, FindClientByCpfGateway {
     private final SaveClientOutPutPort saveClientOutPutPort;
     private final FindClientByCpfOutPutPort findClientByCpfOutPutPort;
 
@@ -13,10 +15,12 @@ public class ClientUseCase {
         this.findClientByCpfOutPutPort = findClientByCpfOutPutPort;
     }
 
+    @Override
     public Client find(String cpf) {
         return this.findClientByCpfOutPutPort.find(cpf);
     }
 
+    @Override
     public Client save(Client client) {
         return this.saveClientOutPutPort.save(client);
     }

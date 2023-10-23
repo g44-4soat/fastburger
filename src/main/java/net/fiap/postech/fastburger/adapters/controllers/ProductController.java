88b1c2c546d35197aac111f59420c346acf365c9
anisberto reis <anisbertoos@gmail.com>
2023-10-22 21:11:04 -0300

    criação de adapter para update de product e configuração de controller criação de objeto de retorno e ajuste de mapper

--- src/main/java/net/fiap/postech/fastburger/adapters/controllers/ProductController.java
+++ src/main/java/net/fiap/postech/fastburger/adapters/controllers/ProductController.java
@@ -2,13 +2,16 @@ package net.fiap.postech.fastburger.adapters.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import net.fiap.postech.fastburger.adapters.configuration.exceptionHandler.BusinessException;
 import net.fiap.postech.fastburger.adapters.persistence.ProductDTO;
+import net.fiap.postech.fastburger.adapters.persistence.ProductResponseDTO;
 import net.fiap.postech.fastburger.adapters.persistence.mapper.ProductMapper;
 import net.fiap.postech.fastburger.application.domain.Product;
 import net.fiap.postech.fastburger.application.domain.enums.CategoryEnum;
 import net.fiap.postech.fastburger.application.ports.inputports.product.FindProductByCategoryGateway;
 import net.fiap.postech.fastburger.application.ports.inputports.product.SaveProductGateway;
+import net.fiap.postech.fastburger.application.ports.inputports.product.UpdateProductGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +28,18 @@ public class ProductController {
 
     private final SaveProductGateway saveProductGateway;
     private final FindProductByCategoryGateway findProductByCategoryGateway;
+
+    private final UpdateProductGateway updateProductGateway;
     private final ProductMapper productMapper;
 
     @Autowired
     public ProductController(SaveProductGateway saveProductGateway,
                              ProductMapper productMapper,
-                             FindProductByCategoryGateway findProductByCategoryGateway) {
+                             FindProductByCategoryGateway findProductByCategoryGateway,
+                             UpdateProductGateway updateProductGateway) {
         this.saveProductGateway = saveProductGateway;
         this.productMapper = productMapper;
+        this.updateProductGateway = updateProductGateway;
         this.findProductByCategoryGateway = findProductByCategoryGateway;
     }
 
@@ -44,11 +51,17 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductDTO>> findProductByCategory(CategoryEnum categoryEnum) {
+    public ResponseEntity<List<ProductResponseDTO>> findProductByCategory(CategoryEnum categoryEnum) {
         var productEntity = this.findProductByCategoryGateway.find(categoryEnum);
-        List<ProductDTO> productDTOList = new ArrayList<>();
-        productEntity.forEach(product -> productDTOList.add(this.productMapper.domainToDTO(product)));
+        List<ProductResponseDTO> productDTOList = new ArrayList<>();
+        productEntity.forEach(product -> productDTOList.add(this.productMapper.domainToDTOResponse(product)));
 
         return ResponseEntity.ok(productDTOList);
+    }
+
+    @PutMapping("/{sku}")
+    public ResponseEntity<ProductResponseDTO> updateProduct(@PathParam("sku") Long sku, @Valid @RequestBody ProductDTO productDTO) {
+        var productUpdated = this.updateProductGateway.update(sku.toString(), this.productMapper.dtoToDomain(productDTO));
+        return ResponseEntity.ok(this.productMapper.domainToDTOResponse(productUpdated));
     }
 }
--- src/main/java/net/fiap/postech/fastburger/adapters/product/UpdateProductAdapter.java
+++ src/main/java/net/fiap/postech/fastburger/adapters/product/UpdateProductAdapter.java
@@ -1,4 +1,36 @@
 package net.fiap.postech.fastburger.adapters.product;
 
-public class UpdateProductAdapter {
+import net.fiap.postech.fastburger.adapters.configuration.exceptionHandler.BusinessException;
+import net.fiap.postech.fastburger.adapters.persistence.mapper.ProductMapper;
+import net.fiap.postech.fastburger.adapters.persistence.repositories.ProductRepository;
+import net.fiap.postech.fastburger.application.domain.Product;
+import net.fiap.postech.fastburger.application.ports.inputports.product.UpdateProductGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateProductAdapter implements UpdateProductGateway {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public UpdateProductAdapter(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public Product update(String sku, Product product) {
+        if (product.getPrice() <= 0) {
+            throw new BusinessException("O valor do produto não pode ser igual ou menor que zero!");
+        }
+
+        var productEntity = this.productRepository.findById(Long.parseLong(sku));
+        var productEntityToSave = this.productMapper.domainToEntity(product);
+
+        if (!productEntity.isPresent())
+            throw new BusinessException("Produto não encontrado.");
+        productEntityToSave.setSKU(productEntity.get().getSKU());
+        return this.productMapper.toDomain(this.productRepository.save(productEntityToSave));
+    }
 }

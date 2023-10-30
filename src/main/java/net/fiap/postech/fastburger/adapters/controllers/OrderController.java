d52a9e22d085ed1854aa2dab9ce8f461303636d6
anisberto reis <anisbertoos@gmail.com>
2023-10-30 10:53:48 -0300

    inclusao de novo use case - find order by status

--- src/main/java/net/fiap/postech/fastburger/adapters/controllers/OrderController.java
+++ src/main/java/net/fiap/postech/fastburger/adapters/controllers/OrderController.java
@@ -6,11 +6,13 @@ import net.fiap.postech.fastburger.adapters.persistence.dto.OrderItemDTO;
 import net.fiap.postech.fastburger.adapters.persistence.dto.OrderRequestDTO;
 import net.fiap.postech.fastburger.adapters.persistence.mapper.OrderMapper;
 import net.fiap.postech.fastburger.application.domain.Order;
+import net.fiap.postech.fastburger.application.domain.enums.StatusOrder;
 import net.fiap.postech.fastburger.application.ports.inputports.order.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,12 +25,14 @@ public class OrderController {
     private final SaveOrderGateway saveOrderGateway;
     private final UpdateOrderGetway updateOrderGetway;
 
+    private final ListOrderByStatusGateway listOrderByStatusGateway;
     private final OrderMapper orderMapper;
 
     @Autowired
     public OrderController(DeleteOrderGateway deleteOrderGateway,
                            ListOrderByNumberGateway listOrderByNumberGateway,
                            OrderMapper orderMapper,
+                           ListOrderByStatusGateway listOrderByStatusGateway,
                            ListOrdersGateway listOrdersGateway, SaveOrderGateway saveOrderGateway, UpdateOrderGetway updateOrderGetway) {
         this.deleteOrderGateway = deleteOrderGateway;
         this.listOrderByNumberGateway = listOrderByNumberGateway;
@@ -36,12 +40,21 @@ public class OrderController {
         this.saveOrderGateway = saveOrderGateway;
         this.updateOrderGetway = updateOrderGetway;
         this.orderMapper = orderMapper;
+        this.listOrderByStatusGateway = listOrderByStatusGateway;
     }
 
     @GetMapping("orderNumber/{orderNumber}")
     public ResponseEntity<OrderDTO> findOrderByNumber(@PathVariable("orderNumber") String orderNumber) {
         Order order = this.listOrderByNumberGateway.listByNumber(orderNumber);
         return ResponseEntity.ok(this.orderMapper.orderToOrderDTO(order));
+    }
+
+    @GetMapping("/{status}")
+    public ResponseEntity<List<OrderDTO>> findOrdersByStatus(@PathVariable("status") StatusOrder statusOrder) {
+        List<Order> orders = this.listOrderByStatusGateway.listByStatus(statusOrder);
+        List<OrderDTO> orderDTOS = new ArrayList<>();
+        orders.forEach(order -> orderDTOS.add(this.orderMapper.orderToOrderDTO(order)));
+        return ResponseEntity.ok(orderDTOS);
     }
 
     @PostMapping("/create")

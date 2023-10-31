35eb447f4160395a3f9e5d1841f834df9abf795c
Felipe Santos <fsantosmg@gmail.com>
2023-10-30 21:43:12 -0300

    vesão 30/10

--- src/main/resources/db/migration/V12__update_tabela_orders.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.orders ADD CONSTRAINT fkm2dep9derpoaehshbkkatam3v FOREIGN KEY (client_id) REFERENCES public.clients(id);
--- src/main/resources/db/migration/V9__criar_tabela_order_itens.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE public.order_itens
-(
-    order_id int8 NOT NULL,
-    itens_id int8 NOT NULL
-);

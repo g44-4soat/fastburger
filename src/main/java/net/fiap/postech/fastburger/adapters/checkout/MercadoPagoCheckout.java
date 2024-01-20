8bb74fe409420837655fbe6fca258e47bc0c56fc
anisberto reis <anisbertoos@gmail.com>
2024-01-20 20:48:08 -0300

    config service checkout mercado pago

---
+++ src/main/java/net/fiap/postech/fastburger/adapters/checkout/MercadoPagoCheckout.java
@@ -0,0 +1,8 @@
+package net.fiap.postech.fastburger.adapters.checkout;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MercadoPagoCheckout {
+
+}
--- src/main/java/net/fiap/postech/fastburger/adapters/feignClients/MercadoPagoService.java
+++ src/main/java/net/fiap/postech/fastburger/adapters/feignClients/MercadoPagoService.java
@@ -1,8 +1,6 @@
 package net.fiap.postech.fastburger.adapters.feignClients;
 
-import net.fiap.postech.fastburger.adapters.feignClients.dto.PayerDTO;
 import net.fiap.postech.fastburger.adapters.feignClients.dto.PaymentDTO;
-import net.fiap.postech.fastburger.adapters.persistence.dto.ClientDTO;
 import net.fiap.postech.fastburger.adapters.persistence.dto.OrderDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,15 +10,15 @@ public class MercadoPagoService {
 
     @Value("${mercado-pago.token}")
     private String bearerToken;
-
     private final MercadoPagoFeignClient mercadoPagoFeignClient;
-
     public MercadoPagoService(MercadoPagoFeignClient mercadoPagoFeignClient) {
         this.mercadoPagoFeignClient = mercadoPagoFeignClient;
     }
 
     public PaymentDTO generateQRCode(OrderDTO orderDTO){
-        PayerDTO payer = new PayerDTO(orderDTO.getClientCPF(), orderDTO.getcl)
+        this.mercadoPagoFeignClient.generateQRCode(null, null, null);
+        //PayerDTO payer = new PayerDTO(orderDTO.getClientCPF(), orderDTO);
+        return null;
     }
 
 }

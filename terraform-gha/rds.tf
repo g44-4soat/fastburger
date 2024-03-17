5f4a9765843a1eb23c54113fabf2cbfb0bdb17cb
Carlos Alvsan <carlos@alvsan.org>
2024-03-16 21:08:00 -0300

    Adcionado um conf exclusido para o gha

---
+++ terraform-gha/rds.tf
@@ -0,0 +1,27 @@
+resource "aws_db_instance" "rds" {
+  db_name        = var.rdsDbName
+  engine         = var.engineRds
+  engine_version = var.engineRdsVersion
+  identifier     = "rds-${var.projectName}"
+  # manage_master_user_password  = true 
+  username                     = var.rdsUser
+  password                     = var.rdsPass
+  instance_class               = var.instanceClass
+  storage_type                 = var.storageType
+  allocated_storage            = var.minStorage
+  max_allocated_storage        = var.maxStorage
+  multi_az                     = false
+  vpc_security_group_ids       = ["${aws_security_group.sg-rds.id}"]
+  db_subnet_group_name         = aws_db_subnet_group.subnet-rds.id
+  apply_immediately            = true
+  skip_final_snapshot          = true
+  publicly_accessible          = false
+  deletion_protection          = false
+  performance_insights_enabled = true
+  backup_retention_period      = 1
+  backup_window                = "00:00-00:30"
+  copy_tags_to_snapshot        = true
+  delete_automated_backups     = true
+}
+
+# Caso queira que o AWS Secrets gerencie a senha master, descomente a linha 6 e comente a linha 8, nesse caso ele vai gerar uma e salvar lá.
---
+++ terraform-gha/task.tf
@@ -0,0 +1,66 @@
+resource "aws_ecs_task_definition" "task" {
+  family = "TSK-${var.projectName}"
+  container_definitions = jsonencode([
+    {
+      name      = "${var.projectName}"
+      essential = true,
+      image     = "${aws_ecr_repository.repository.repository_url}:latest",
+      command   = ["java", "-jar", "/app/target/fastburger-0.2.1-SNAPSHOT.jar", "--spring.config.name=docker"]
+      environment = [
+        {
+          name = "SPRING_DATASOURCE_URL"
+          value = "jdbc:postgresql://${aws_db_instance.rds.endpoint}/${aws_db_instance.rds.db_name}"
+        },
+        {
+          name  = "SPRING_DATASOURCE_USERNAME"
+          value = "${var.rdsUser}"
+        },
+        {
+          name  = "SPRING_DATASOURCE_PASSWORD"
+          value = "${var.rdsPass}"
+        },
+        {
+          name  = "SPRING_PORT"
+          value = "${var.spring_port}"
+        },
+        {
+          name  = "MERCADO_TOKEN"
+          value = "${var.mercado_token}"
+        },
+        {
+          name  = "TOKEN_FALLBACK"
+          value = "${var.token_fallback}"
+        },
+        {
+          name  = "MERCADO_API"
+          value = "${var.mercado_api}"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "${aws_cloudwatch_log_group.cloudwatch-log-group.name}"
+          awslogs-region        = "${var.regionDefault}"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+  network_mode = "awsvpc"
+
+  requires_compatibilities = ["FARGATE"]
+
+  execution_role_arn = "arn:aws:iam::${var.AWSAccount}:role/ecsTaskExecutionRole-1"
+
+  memory = "4096"
+  cpu    = "2048"
+
+  depends_on = [aws_db_instance.rds]
+}
